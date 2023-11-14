88cd4724b12fffedd3a992cbf76dcf87c2b611c7
SeeNoFun <119578540+SeeNoFun@users.noreply.github.com>
2023-11-14 20:20:45 +0500

    Notification component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { Notification } from './components';
+import { Status } from './types';
 
 function App() {
+  const [status, setStatus] = useState<Status>('unactive')
+  const [isModalShown, setIsModalShown] = useState<boolean>(false)
+  const [isFetching, setIsFetching] = useState(false)
+
+  // симулция запроса
+  const simulateServer = () => {
+    return new Promise<void>((resolve, reject) => {
+
+      if (Math.random() > 0.5) { 
+        return resolve();
+      }
+      const t: NodeJS.Timeout = setTimeout(() => {
+        reject();
+        return clearTimeout(t);
+      }, 2000);
+    });
+  };
+  // обработчик события click
+  const handleClick:React.MouseEventHandler<HTMLButtonElement> = (e) => {
+    e.preventDefault()
+    setIsFetching(true)
+    simulateServer()
+      .then(() => {
+        setIsModalShown(true)
+        setStatus('success')
+      })
+      .catch(() => {
+        setIsModalShown(true)
+        setStatus('error')
+      })
+      .finally(() => setIsFetching(false))
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className='test-notification__btn' disabled={isFetching} onClick={handleClick}>
+        Test notification
+      </button>
+
+      {isModalShown && 
+        <Notification 
+          status={status} 
+          label={status === 'success' ? 'Успешно' : 'Изменения не сохранены'} 
+          text={status === 'success' ? 'Изменения успешно сохранены' : 'Потеря интернет соединения'}
+          setIsShown={setIsModalShown}
+        />}
     </div>
   );
 }
---
+++ src/components/Notification/Notification.tsx
@@ -0,0 +1,87 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { memo, useEffect, useRef} from "react"
+import './_Notification.css'
+import { Status } from "../../types"
+
+interface NotificationProps {
+    status: Status,
+    label: string,
+    text: string,
+    setIsShown: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const Notification = ({label, status, text, setIsShown}: NotificationProps) => {
+    let barWidth = 0 // <-- текущая ширина load-bar`a в %
+    const animationSpeed = 30;// <-- скорость анимации
+
+    const bar = useRef<HTMLDivElement>(null) //<-- ссылка на элемент .bar
+    const timeoutRefId = useRef<NodeJS.Timeout>() //
+    const intervalRefId = useRef<NodeJS.Timer>() //
+
+
+    // Функция анимации(увеличивает ширину barWidth на 1)
+    const animate = () => {
+        barWidth++
+        if(bar.current){ 
+            bar.current.style.width = `${barWidth}%`
+        } else {
+            clearTimer(); 
+            alert('Please reload the page')
+        }
+    };
+    // установка и очистка setTimoue и setInterval 
+    function setTimer() {
+        timeoutRefId.current = setTimeout(() => {
+            setIsShown(false)
+        }, 3000)
+        intervalRefId.current = setInterval(() => {
+            if (barWidth === 100) {
+                setIsShown(false)
+                clearTimer()
+            } else {
+                animate();
+            }
+        }, animationSpeed);
+    }
+    function clearTimer() {
+        clearTimeout(timeoutRefId.current);
+        clearInterval(intervalRefId.current);
+    }
+    // обработчики наведения мыши на уведомление
+    const onMouseLeaveHandler = () => {
+        setTimer()
+    }
+    const onMouseEnterHandler:React.MouseEventHandler<HTMLDivElement> = (e) => {
+        clearTimer()
+    }
+
+    useEffect(() => {   
+        setTimer() 
+        return () => {
+            clearTimer()
+        }
+    }, [status])
+  return (
+        <div 
+            className="notification"
+            onMouseOver={onMouseEnterHandler} 
+            onMouseLeave={onMouseLeaveHandler}
+        >
+            <div className="notification-icon__container">
+                {status === 'success' 
+                    ? <img src='/images/icons/success.svg' alt='success'/>
+                    : <img src='/images/icons/error.svg' alt='success'/>
+                }
+            </div>
+            <div className="notification-content">
+                <h3 className="notification__label">{label}</h3>
+                <p className="notification__text">{text}</p>
+                <div className="progress">
+                    <div ref={bar} className="bar"/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default memo(Notification)
